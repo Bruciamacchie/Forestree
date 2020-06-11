e5c8989b90121e169b91d69138384ee42ce173c1
MaxBruciamacchie <max.bruciamacchie@agroparistech.fr>
2020-06-11 09:32:30 +0200

    Recherche voisins

---
+++ R/JP2toTIF.R
@@ -0,0 +1,28 @@
+#' Conversion JP2 to TIF
+#'
+#' @description Cette fonction convertit tous les fichiers au format JP2 contenus dans un dossier en format <<tif.
+#'
+#' @import tcltk
+#' @import gdalUtils
+#'
+#' @author Bruciamacchie Max
+#'
+#' @examples
+#' JP2toTIF()
+#'
+#' @export
+
+JP2toTIF <- function() {
+  rep <- tk_choose.dir("Choisir un dossier contenant des fichiers .JP2")
+  fichiers <- list.files(rep, pattern="\\.jp2$")
+  if (length(fichiers) == 0) {
+    stop("Le dossier ne contient aucun fichier au format .jp2")
+  }
+  cat(paste("Le dossier contient",length(fichiers),"fichiers au format .jp2"))
+  for(i in 1:length(fichiers)) {
+    input <- paste0(rep,"/", fichiers[i])
+    out <- paste0(rep,"/", tools::file_path_sans_ext(fichiers[i]),".tif")
+    gdal_translate(input,out)
+    print(paste("Conversion",fichiers[i],"........ terminée"))
+  }
+}
---
+++ R/voisins.R
@@ -0,0 +1,55 @@
+#' Recherche voisins
+#'
+#' @description Recherche de tous les polygones voisins d'un polygone.
+#'
+#' @import sf
+#'
+#' @param shp couche géoréférencée contenant des POLYGON ou MULTIPOLYGON.
+#'
+#' @return La fonction renvoie l'objet sf fournit en entrée avec deux géométries supplémentaires,
+#' l'une correspondant pour chaque polygone à l'union des voisins (geom2),
+#' l'autre à l'union du polygone avec ses voisins (geom1).
+#'
+#' @author Max Bruciamacchie
+#'
+#' @examples
+#' nc = st_read(system.file("shape/nc.shp", package="sf"))
+#' shp <- voisins(nc)
+#'
+#' par(mfrow=c(1,2))
+#' st_geometry(shp) <- "geom1"
+#' plot(st_geometry(shp[100,]),border="blue", lwd=4)
+#' plot(st_geometry(nc[100,]), col="grey90", add=T)
+#' plot(st_geometry(nc), border="red", lwd=1.5, add=T)
+#'
+#' st_geometry(shp) <- "geom2"
+#' plot(st_geometry(shp[100,]),border="blue", lwd=4)
+#' plot(st_geometry(nc[100,]), col="grey90", add=T)
+#' plot(st_geometry(nc), border="red", lwd=1.5, add=T)
+#' par(mfrow=c(1,1))
+#'
+#' @export
+
+
+Voisins <- function(shp){
+  if (!inherits(shp, "sf")) {
+    stop("La fonction nécessite en entrée un objet sf.")
+  }
+
+  geom1 = list()
+  geom2 = list()
+  for(i in 1:nrow(shp)) {
+    geom1[[i]] = st_union(shp[shp[i, ], ])
+    geom2[[i]] = st_difference(geom1[[i]], shp[i, ])
+  }
+  geom1 = do.call(c, geom1)
+  geom2 = do.call(c, geom2)
+  shp$geom1 = geom1
+  shp$geom2 = geom2
+  return(shp)
+}
+
+
+
+
+
